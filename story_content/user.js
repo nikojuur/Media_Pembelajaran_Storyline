eb57f7909abfdad9c4c687ff2cbaac0dcb535068
nikojuur <njuwur@gmail.com>
2025-05-31 21:37:35 +0800

    Initial comit

---
+++ story_content/user.js
@@ -0,0 +1,19 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6j5Ko6qXfxi":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = dokument.getElementByld('bgongku');
+audio.scr="musikku.mp3";
+audio.lord();
+audio.play();
+audio.volume=0,6;
+}
+
